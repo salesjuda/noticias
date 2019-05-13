1beed75491dfbf1222c10939158edbf12a0f92ca
Judá Sales <juda_sales@hotmail.com>
2019-05-13 13:14:12 -0300

    teste 2.0

--- noticias.js
+++ noticias.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 var servidor = http.createServer(function(req, res){
-    res.end("<html><body>Olá Node.js</body></html>")
+    res.end("<html><body>Olá mundo do Node.js</body></html>")
 });
 servidor.listen(3000);
