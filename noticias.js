0988bad187f98cbe9c456e8ca9fe6783ab8eb595
Judá Sales <juda_sales@hotmail.com>
2019-05-13 14:49:02 -0300

    teste

--- noticias.js
+++ noticias.js
@@ -1,6 +1,16 @@
 var http = require('http');
 
-var servidor = http.createServer(function(req, res){
-    res.end("<html><body>Olá mundo do Node.js</body></html>")
+var server = http.createServer(function(req,res){
+    var categoria = req.url;
+    if(categoria == '/tecnologia'){
+        res.end("<html><body>Notícias de Tecnologia</body></html>");
+    }else if(categoria == '/moda'){
+        res.end("<html><body>Notícias de Moda</body></html>");
+    }else if(categoria == '/beleza'){
+        res.end("<html><body>Notícias de Beleza</body></html>");
+    }else{
+        res.end("<html><body>Portal de Notícias</body></html>");
+    }
 });
-servidor.listen(3000);
+
+server.listen(3000);
