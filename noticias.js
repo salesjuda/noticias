46435056dc1c5ccf9b5c71af521abb0846540dcc
Judá Sales <juda_sales@hotmail.com>
2019-05-13 14:54:12 -0300

    teste

--- noticias.js
+++ noticias.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(req,res){
     var categoria = req.url;
     if(categoria == '/tecnologia'){
-        res.end("<html><body>Notícias de Tecnologia</body></html>");
+        res.end("<html><body>Notícias de Tecnologiaa</body></html>");
     }else if(categoria == '/moda'){
         res.end("<html><body>Notícias de Moda</body></html>");
     }else if(categoria == '/beleza'){
