82c2e1bd8d91ea6b848d558506b0ec2d34c8dbff
Judá Sales <juda_sales@hotmail.com>
2019-05-16 01:43:00 -0300

    aula 24 criando a conexão com o mysql e recuperan

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,21 @@
 module.exports = function(app){
 
     app.get('/noticias', function(req, res){
-        res.render('noticias/noticias');
+
+    var mysql = require('mysql');
+
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'portal_noticias'
+    });
+
+    connection.query('SELECT * FROM noticias', function(error, result){
+        res.send(result);
+    });
+
+    
+        //res.render('noticias/noticias');
     });
 };
