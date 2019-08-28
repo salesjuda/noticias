41807c835b9d32c2269856ce06ec27f90ec9426b
Judá Sales <juda_sales@hotmail.com>
2019-08-28 00:57:25 -0300

    seção 10 aula 44

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,6 +7,20 @@ module.exports = function(app){
     app.post('/noticias/salvar', function(req, res){
         var noticia = req.body;
 
+        req.assert('titulo','Titulo é obrigatorio').notEmpty();
+        req.assert('resumo','Resumo é obrigatorio').notEmpty();
+        req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor','Autor é obrigatorio').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('noticia','Noticia é obrigatorio').notEmpty();
+
+        var erros = req.validationErrors();
+
+        if(erros){
+            res.render('admin/form_add_noticia');
+            return;
+        }
+
         var connection = app.config.dbConnection(); 
         var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
